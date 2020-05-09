5f8ff9d8f8fb24f23612135918bcdbe31f6b1667
Rupam55 <rupamkarmakar55@gmail.com>
2020-05-09 15:28:23 +0530

    second commit

--- coursera/data structures/week1-1.cpp
+++ coursera/data structures/week1-1.cpp
@@ -1,49 +1,64 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+int c = 0;
+vector<vector<int>> vect;
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;   // Initialize largest as root
+    int l = 2 * i + 1; // left = 2*i + 1
+    int r = 2 * i + 2; // right = 2*i + 2
+
+    // If left child is larger than root
+    if (l < n && arr[l] < arr[largest])
+        largest = l;
+
+    // If right child is larger than largest so far
+    if (r < n && arr[r] < arr[largest])
+        largest = r;
+
+    // If largest is not root
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        c++;
+        vector<int> v1;
+        v1.push_back(largest);
+        v1.push_back(i);
+        vect.push_back(v1);
+        heapify(arr, n, largest);
+    }
+}
+
+void buildHeap(int arr[], int n)
+{
+    int startIdx = (n / 2) - 1;
+
+    for (int i = startIdx; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+}
+
+void printHeap(int arr[], int n)
+{
+    for (int i = 0; i < vect.size(); ++i){
+        cout<<vect[i][0] <<" "<<vect[i][1] <<"\n";
+    }
+}
+
 int main()
 {
-    string str;
-    int i = 0, check = true, a;
-    char top;
-    cin >> str;
-    stack<char> s;
-    while (i < str.length())
-    {
-        if (s.empty())
-            a = i;
-        if (str[i] == '[' || str[i] == '{' || str[i] == '(')
-            s.push(str[i]);
-        else
-        {
-            if ((str[i] == ']' || str[i] == '}' || str[i] == ')'))
-            {
-                if (s.empty())
-                {
-                    check = false;
-                    break;
-                }
-                else
-                {
-                    top = s.top();
-                    s.pop();
-                    if ((top == '[' && str[i] != ']') || (top == '{' && str[i] != '}') || (top == '(' && str[i] != ')'))
-                    {
-                        check = false;
-                        break;
-                    }
-                }
-            }
-        }
-        i += 1;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-    if (check && s.empty())
-        cout << "Success" << endl;
-    else
-    {
-        if ((i >= str.size()) && !s.empty())
-            cout << a + 1 << endl;
-        else
-            cout << i + 1 << endl;
-    }
+    buildHeap(arr, n);
+    cout << c << "\n";
+    printHeap(arr, n);
     return 0;
 }
--- coursera/data structures/week1-3.cpp
+++ coursera/data structures/week1-3.cpp
@@ -1,34 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+class JobQueue
+{
+private:
+    int num_workers_;
+    vector<int> jobs_;
+
+    vector<int> assigned_workers_;
+    vector<long long> start_times_;
+    struct t
+    {
+        int id;
+        long long next_free_time;
+        t(int id, long long t = 0) : id(id), next_free_time(t){};
+        void addtime(long long t) { next_free_time += t; }
+        friend bool operator<(const t &b, const t &a)
+        {
+            if (b.next_free_time != a.next_free_time)
+                return b.next_free_time > a.next_free_time;
+            else
+                return b.id > a.id;
+        }
+    };
+    void write() const
+    {
+        for (int i = 0; i < jobs_.size(); ++i)
+        {
+            cout << assigned_workers_[i] << " " << start_times_[i] << "\n";
+        }
+    }
+
+    void read()
+    {
+        int m;
+        cin >> num_workers_ >> m;
+        jobs_.resize(m);
+        for (int i = 0; i < m; ++i)
+            cin >> jobs_[i];
+    }
+
+    void assignwork()
+    {
+        assigned_workers_.resize(jobs_.size());
+        start_times_.resize(jobs_.size());
+        priority_queue<t, vector<t>> q;
+        for (int i = 0; i < num_workers_; i++)
+        {
+            q.push(t(i));
+        }
+        for (int i = 0; i < jobs_.size(); ++i)
+        {
+            int duration = jobs_[i];
+            t td = q.top();
+            q.pop();
+            assigned_workers_[i] = td.id;
+            start_times_[i] = td.next_free_time;
+            td.next_free_time += duration;
+            q.push(td);
+        }
+    }
+
+public:
+    void Solve()
+    {
+        read();
+        assignwork();
+        write();
+    }
+};
+
 int main()
 {
-    int n,m;
-    cin>>n;
-    int arr[n];
-    for (int i = 0; i <n; i++){
-        cin>>arr[i];
-    }
-    cin>>m;
-    deque<int> Qi(m);
-    int i; 
-	for (i = 0; i <m; ++i) {
-        while ((!Qi.empty()) && arr[i] >= arr[Qi.back()]){
-            Qi.pop_back();
-        }
-
-        Qi.push_back(i);
-    }
-    for (; i <n; ++i) { 
-		cout << arr[Qi.front()] << " "; 
-
-		while ((!Qi.empty()) && Qi.front() <= i - m) 
-			Qi.pop_front(); 
-
-		while ((!Qi.empty()) && arr[i] >= arr[Qi.back()]) 
-			Qi.pop_back(); 
- 
-		Qi.push_back(i); 
-	}
-    cout << arr[Qi.front()];
+    std::ios_base::sync_with_stdio(false);
+    JobQueue job_queue;
+    job_queue.Solve();
+    return 0;
 }
-
