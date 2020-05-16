4cfce2d389466b0ba600f2e58ff5a8596ac46265
Rupam55 <rupamkarmakar55@gmail.com>
2020-05-16 23:35:07 +0530

    day 16 may

--- coursera/data structures/week1-1.cpp
+++ coursera/data structures/week1-1.cpp
@@ -2,50 +2,52 @@
 
 using namespace std;
 
-#define Time() cerr << clock() * 1000 / CLOCKS_PER_SEC << "ms\n"
-#define show(x) cerr << #x << " = " << (x) << '\n'
+void inorder(int arr[][3],int pos){
+    if(arr[pos][1]!=-1){
+        inorder(arr,arr[pos][1]);
+    }
+    cout << arr[pos][0]<<" ";
+    if(arr[pos][2]!=-1){
+        inorder(arr, arr[pos][2]);
+    }
+}
+void preorder(int arr[][3],int pos){
+    if(arr[pos][1]!=-1){
+        preorder(arr,arr[pos][1]);
+    }
+    if(arr[pos][2]!=-1){
+        preorder(arr, arr[pos][2]);
+    }
+    cout << arr[pos][0]<<" ";
+    
+}
+void postorder(int arr[][3], int pos)
+{
+    cout << arr[pos][0]<<" ";
+    if (arr[pos][1] != -1)
+    {
+        postorder(arr, arr[pos][1]);
+    }
+    if (arr[pos][2] != -1)
+    {
+        postorder(arr, arr[pos][2]);
+    }
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n,index;
-    bool flag=false;
+    
+    int n;
     cin>>n;
-    string s,name;
-    unordered_map<int, string> mp;
-    while(n--){
-        cin>>s;
-        if(s=="add"){
-            cin>>index>>name;
-            // mp.insert({index,name});
-            mp[index]=name;
-        }else if(s=="find"){
-            cin>>index;
-            if (mp[index]==""){
-                cout<<"not found"<<"\n";
-            }else{
-                cout << mp[index] << "\n";
-            }
-
-            // flag=false;
-            // for (auto x : mp){
-            //     if(x.first==index){
-            //         flag=true;
-            //         cout<<x.second<<"\n";
-            //     }
-            // }
-            // if(flag==false){
-            //     cout<<"not found"<<"\n";
-            // }
-        }else if(s=="del"){
-            cin>>index;
-            mp.erase(index);
-            // for (auto x : mp){
-            //     if(x.first==index){
-            //         mp[index]="not found";
-            //     }
-            // }
-        }
+    int arr[n][3];
+    for (int i = 0; i < n; i++){
+        cin>>arr[i][0]>>arr[i][1]>>arr[i][2];
     }
+    inorder(arr,0);
+    cout<<"\n";
+    postorder(arr, 0);
+    cout<<"\n";
+    preorder(arr, 0);
 }
--- coursera/data structures/week1-3.cpp
+++ coursera/data structures/week1-3.cpp
@@ -1,25 +1,76 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
+bool flag = true;
+int n;
+
+bool find(int arr[][3], int pos, int min, int max)
+{
+    if (arr[pos][1] == -1 || arr[pos][1] == -1)
+    {
+        return true;
+    }
+    if (arr[pos][1] != -1)
+    {
+        if (arr[arr[pos][1]][0] < arr[pos][0] && arr[arr[pos][1]][0] > min)
+        {
+            // cout<<min<<" "<<max<<"\n";
+            find(arr, arr[pos][1], min, arr[pos][0]);
+        }
+        else
+        {
+            // cout << arr[arr[pos][2]][0] << " " << max << " " << 1<<"  ";
+            return false;
+        }
+    }
+    if (arr[pos][2] != -1)
+    {
+        if (arr[arr[pos][2]][0] < max && arr[arr[pos][2]][0] >= arr[pos][0])
+        {
+            // cout<<min<<" "<<max<<"\n";
+            find(arr, arr[pos][2], arr[pos][0], max);
+        }
+        else
+        {
+            // cout << pos<< " "<< arr[arr[pos][2]][0]<<" " << min << " "<< max<< " " << 2<<"  ";
+            return false;
+        }
+    }
+    // if(pos==-1){return true;}
+    // if(arr[pos][0]>=max||arr[pos][0]<min){
+    //     return false;
+    // }
+    // return find(arr,arr[pos][1],min,arr[pos][0]) && find(arr,arr[pos][2],arr[pos][0],max);
+}
 
 int main()
 {
-    string str,str1;
-    cin>>str1>>str;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    size_t found = str.find(str1);
-    while(found != string::npos){
-        cout << found << " ";
-        found = str.find(str1, found + 1);
+    cin >> n;
+    int arr[n][3];
+    if (n == 0 || n == 1)
+    {
+        cout << "CORRECT";
     }
-    // char arr[] = str1;
-    
-    // while (found != string::npos){
-    //     cout << "Next occurrence is " << found << endl;
-    //     found = str.find(arr, found + 1);
-    // }
-    // return 0;
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
+        }
+        int min = INT_MIN, max = INT_MAX;
+        flag = find(arr, 0, min, max);
+        if (flag == false)
+        {
+            cout << "INCORRECT";
+        }
+        else
+        {
+            cout << "CORRECT";
+        }
+    }
 }
 
-// week 4 ques 4
+// Kill Me Plz
