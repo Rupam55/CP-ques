aae336cf1774470b42cf7dd8d293aebe8cd65736
Rupam55 <rupamkarmakar55@gmail.com>
2020-06-11 17:39:25 +0530

    11 june

--- CP/cp1.cpp
+++ CP/cp1.cpp
@@ -2,30 +2,51 @@
 
 using namespace std;
 
-typedef unsigned long long ull;
-typedef long long ll;
-typedef long double ld;
+
+
+/*
+ * Complete the 'decryptPassword' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
 
 int main()
 {
-    int n,a=0,g=0,at=0,gt=0;
-    string s="";
-    cin>>n;
-    for (int i = 0; i < n; i++)
-    {
-        cin>>a>>g;
-        if(abs((at+a)-gt)<=abs((gt+g)-at)){
-            s+='A';
-            at+=a;
+    string s,n,res;
+    cin>>s;
+    int i=0;
+    while(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9'){
+        n+=s[i];
+        i++;
+        cout<<"1.2";
+    }
+    cout<<n.length()<<s.length();
+    int j=n.length()-1;
+    while(i<s.length()){
+        if(isupper(s[i])){
+            if(islower(s[i+1])){
+                res.push_back(s[i+1]);
+                res.push_back(s[i]);
+                i+=3;
+                cout<<1;
+            }
+        }else if(s[i]=='0'){
+            // if(n.length()==0){
+            //     res.push_back(n[i]);
+            //     i++;
+            //     cout<<2;    
+            // }else{
+            //     res.push_back(n[j]);
+            //     j--;
+            //     i++;
+            //     cout<<3;
+            // }
         }else{
-            s += 'G';
-            gt += g;
+            res.push_back(s[i]);
+            i++;
         }
     }
-    if(abs(at-gt)<=500){
-        cout<<s;
-    }else{
-        cout<<"-1";
-    }  
+    cout<<res;
+    return 0;
 }
-//ladder ques 33
--- CP/cp2.cpp
+++ CP/cp2.cpp
@@ -1,32 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef unsigned long long ull;
 typedef long long ll;
-typedef long double ld; 
+typedef long double ld;
 
 int main()
 {
-	string s;
-	cin>>s;
-	int n=0,m=0;
-	for (int i = 0; i < s.length(); i++)
+	string s, res="";
+	cin >> s;
+	int i = 0, n = 0;
+	
+	for (i = 0; i < s.length(); i++)
 	{
-		if(s[i]=='4'){
+		if (s[i] == '1' || s[i] == '2' || s[i] == '3' || s[i] == '4' || s[i] == '5' || s[i] == '6' || s[i] == '7' || s[i] == '8' || s[i] == '9')
+		{ 
 			n++;
-		}else if(s[i]=='7'){
-			m++;
+		}
+		else
+		{
+			if (isupper(s[i]))
+			{
+				if (islower(s[i + 1]))
+				{
+					if(s[i+2]=='*'){
+						res.push_back(s[i + 1]);
+						res.push_back(s[i]);
+						i += 2;
+					}else
+					{
+						res.push_back(s[i]);
+					}
+				}else
+					{
+						res.push_back(s[i]);
+					}
+			}else if (s[i] == '0')
+			{
+				if (n == 0)
+				{
+					res += '0';
+				}
+				else
+				{
+
+					res.push_back(s[n - 1]);
+					n--;
+				}
+			}
+			else
+			{
+				res.push_back(s[i]);
+			}
 		}
 	}
-	if(m==0&&n==0){
-		cout<<"-1";
-	}else if(m==n){
-		cout<<"4";
-	}else if(m>n){
-		cout<<"7";
-	}else{
-		cout<<"4";
-	}
-	
+	cout << res;
 }
-//ladder ques 27
