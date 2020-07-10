d73e2449baf5dc2276f9597770fc964fcb925bc3
rupam karmakar <rupamkarmakar55@gmail.com>
2020-07-10 12:55:26 +0530

    f

--- CP/cp1.cpp
+++ CP/cp1.cpp
@@ -11,13 +11,11 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    string s;
-    getline(cin,s);
-    set<char>st;
-    for (int i = 0; i < s.length(); i++)
-    {
-        st.insert(s[i]);
-    }
-    (st.size()%2==0)?cout<<"CHAT WITH HER!":cout<<"IGNORE HIM!";
+    ll a,b,low,high;
+    cin>>a>>b;
+    low=min(a,b);
+    high=max(a,b);
+    
+    return 0;
 }
-// cd string 236/A
+// cd 4c registration system
